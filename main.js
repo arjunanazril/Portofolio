eba47a0a1ba920428b3b727ef9790907f28c1a65
arjunanazril <arjunanazril486@gmail.com>
2025-04-30 18:14:09 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+// Smooth scroll to 'Tentang Saya' section when button is clicked
+document.getElementById('scrollToAboutBtn').addEventListener('click', function() {
+    document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+  });
+  
---
+++ script.js
@@ -0,0 +1,19 @@
+let index = 0;
+        function moveSlide(step) {
+            const carousel = document.querySelector('.carousel');
+            const cards = document.querySelectorAll('.card');
+            const buttons = document.querySelectorAll('.nav-button');
+            
+            // Sembunyikan tombol sejenak
+            buttons.forEach(button => button.classList.add('hidden'));
+            
+            index += step;
+            if (index < 0) index = cards.length - 1;
+            if (index >= cards.length) index = 0;
+            carousel.style.transform = `translateX(-${index * 320}px)`;
+            
+            // Tampilkan tombol kembali setelah animasi selesai
+            setTimeout(() => {
+                buttons.forEach(button => button.classList.remove('hidden'));
+            }, 500);
+        }
